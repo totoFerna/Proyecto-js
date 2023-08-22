2a408395d4221007f2d7e52b3930fcc88786b009
Tomas Fernandez <tomas.fernandez1606@gmail.com>
2023-08-21 22:22:20 -0300

    calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,12 +1,30 @@
-function calcularPrecioConImpuestos() {
-    
-    const precioInicial = parseFloat(document.getElementById('precio-inicial').value);
-    const tasaImpuesto = 0.75; 
-    
-    const impuestosCalculados = precioInicial * tasaImpuesto;
-    const precioFinal = precioInicial + impuestosCalculados;
+function realizarOperaciones() {
+    const num1 = parseFloat(prompt("Ingrese el primer número:"));
+    const num2 = parseFloat(prompt("Ingrese el segundo número:"));
 
-    document.getElementById('precio-final').textContent = `El precio final, incluyendo impuestos, es $${precioFinal.toFixed(2)}`;
+    const operacion = prompt("Ingrese la operación deseada (+, -, *, /, %):");
+
+    let resultado;
+    switch (operacion) {
+        case "+":
+            resultado = num1 + num2;
+            break;
+        case "-":
+            resultado = num1 - num2;
+            break;
+        case "*":
+            resultado = num1 * num2;
+            break;
+        case "/":
+            resultado = num1 / num2;
+            break;
+        case "%":
+            resultado = (num1 * num2) / 100;
+            break;
+        default:
+            alert("Operación no válida");
+            return;
+    }
+
+    alert(`Resultado de ${num1} ${operacion} ${num2} = ${resultado}`);
 }
-
-document.getElementById('calcular-btn').addEventListener('click', calcularPrecioConImpuestos);
